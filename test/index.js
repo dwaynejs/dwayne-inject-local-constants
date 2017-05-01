ec68a666c017516c34f3b38a759a84fb7b9ea1f1
droooney <d.vor7@mail.ru>
2017-05-01 20:46:07 +0500

    Use dwayne-test-utils.

--- test/index.js
+++ test/index.js
@@ -1,53 +1,25 @@
-import { Block, initApp, doc } from 'dwayne';
+import { initApp } from 'dwayne-test-utils';
 import { strictEqual } from 'assert';
+import { Block } from 'dwayne';
 import injectLocalConstants from '../src';
-
-let afterRender;
-let currentApp;
 
 let a = {};
 let b = {};
+let remove;
 
-class App extends Block {
-  constructor(opts) {
-    super(opts);
-
-    currentApp = this;
-  }
-
-  afterRender() {
-    afterRender();
-  }
-}
-
-Block.block('App', App.wrap(
+const App = Block.wrap(
   injectLocalConstants({
     a,
     b
   })
-));
+);
 
 describe('it should test injectLocalConstants wrapper', () => {
-  before((done) => {
-    let isDone = 0;
-
-    afterRender = () => {
-      doneAll();
-    };
-
-    initApp('App', doc.create('div'));
-
-    setTimeout(doneAll, 0);
-
-    function doneAll() {
-      if (++isDone === 2) {
-        done();
-      }
-    }
-  });
+  before(remove = initApp(App));
+  after(remove);
 
   it('should test injecting locals', () => {
-    strictEqual(currentApp.a, a);
-    strictEqual(currentApp.b, b);
+    strictEqual($app.a, a);
+    strictEqual($app.b, b);
   });
 });
